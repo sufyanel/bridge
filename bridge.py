e5fbd66e69b2b99de575d2fbe74ee3e249f1c40a
Sufyan Elahi <sufyanelahi@gmail.com>
2022-07-12 06:01:37 +0500

    removed card class

--- bridge.py
+++ bridge.py
@@ -20,34 +20,16 @@ RANKS = {1: "Ace",
          12: "Queen",
          13: "King"}
 
-
-class Card:
-    '''card: has two attributes, a suit and a rank'''
-
-    def __init__(self, suit, rank):
-        '''Creates a card with a valid suit and rank'''
-        # checks against the suits and ranks constants, if not in the valid ones, raise exception
-        if suit not in SUITS.keys():
-            raise Exception("Not a valid suit")
-        if rank not in RANKS.keys():
-            raise Exception("Not a valid rank")
-
-        # if suit and rank is valid, create the card
-        self.suit = suit
-        self.rank = rank
-
-    def __str__(self):
-        '''Method to turn card to a string'''
-        return RANKS[self.rank]+" of "+SUITS[self.suit]
-
-
 class Stack:
     '''A stack of cards'''
     cards = []
 
-    def __str__(self) -> str:
+    def __str__(self):
         '''Returns string of all cards in the stack, by complete name'''
-        return ('\n'.join(map(str, self.cards)))
+        # getting full names of ranks and suits from the constant RANKS & SUITS dictionaries,
+        # lambda function formats them pretty, like Eight of Spades
+        # using the join, all list is converted to a single string with New line character: '\n'
+        return '\n'.join(map(lambda card: RANKS[card[1]]+' of '+SUITS[card[0]], self.cards))
 
     def shuffle(self):
         '''Shuffles the stack'''
@@ -55,7 +37,10 @@ class Stack:
 
     def sort(self):
         '''Sorts cards in the stack'''
-        self.cards = self.cards.sort
+        # Sort Rank wise 
+        self.cards.sort(key=lambda c:c[1])
+        # Sort Suit Wise 
+        self.cards.sort(key=lambda c:c[0])
 
 class Deck(Stack):
     '''A stack of all 52 cards of 13 unique ones in each of four suits'''
@@ -64,16 +49,12 @@ class Deck(Stack):
         '''Initializing all 52 cards'''
         for suit in SUITS.keys():
             for rank in RANKS.keys():
-                self.cards.append(Card(suit, rank))
+                self.cards.append((suit, rank))
 
 
 def main():
     deck = Deck()
-    print("here's my deck:")
-    print(deck)
-    print("Shuffling .........................")
-    deck.shuffle()
-    print(len(deck.cards))
+    print(deck) 
 
 
 if __name__ == '__main__':
